bb76c13acf821812c40ae0db483ccf6492505a3d
HuongDinh <dinhhuong105@gmail.com>
2017-08-15 11:53:47 +0700

    upload plugis - fixes variable name for javascript

--- wp-content/plugins/spc-threads/views/js/notice-board.js
+++ wp-content/plugins/spc-threads/views/js/notice-board.js
@@ -58,7 +58,7 @@ jQuery(document).ready(function($) {
         // check max file size
         if (file_data.size > (1024000*max_upload_file_size)) {
             jQuery('.threadFormArea.inputForm .threadList li').find('.upload_error').remove();
-            jQuery('.threadFormArea.inputForm .threadList li').last().append('<div class="upload_error">※画像のサイズは'+ max_upload_file_size24 +'MBまで等</div>');
+            jQuery('.threadFormArea.inputForm .threadList li').last().append('<div class="upload_error">※画像のサイズは'+ max_upload_file_size +'MBまで等</div>');
             jQuery('#formComment li.comment-content').find('.upload_error').remove();
             jQuery('#formComment li.comment-content').append('<div class="upload_error">※画像のサイズは'+ max_upload_file_size +'MBまで等</div>');
             return false;
